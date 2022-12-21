2b055f57cb76851e25dda0b522fa056442358931
Stephen Lam <justslamthings@gmail.com>
2022-12-21 00:25:22 -0500

    sending index and show data

--- views/index.jsx
+++ views/index.jsx
@@ -3,7 +3,7 @@ const Default = require('./layouts/Default')
 
 function Index ({breads, title})  {
     return (
-<Default>
+<Default title={title}>
   <h2>Index Page</h2>
   {/* <p>I have {breads[0].name} bread!</p> */}
   {/* This is a JSX comment. */}
@@ -21,13 +21,7 @@ function Index ({breads, title})  {
 }
   </ul>
 </Default>
-
-
-
-
     )
 }
 
-
 module.exports = Index
-
